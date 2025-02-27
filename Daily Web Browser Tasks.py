6f428319afd6a393ed719c1a91f4a5c17c5d33a8
EEM Byte <127450516+EEMdata@users.noreply.github.com>
2025-02-26 21:09:31 -0500

    Update Daily Web Browser Tasks.py
    Updated it for cmd use

--- Daily Web Browser Tasks.py
+++ Daily Web Browser Tasks.py
@@ -1,19 +1,32 @@
-## Open Daily Tasks in browsers
-'''
 import webbrowser
 
-urlsM = [
-    #[Insert name of URL link] 
-    'https://app.example.com/tenant/YourOrganization/segment',
+# Define URL lists for each section
+sections = {
+    "Mon1": [
+        'https://www.xxxxxxxxxxxx.com/xxxxxxxxxxxxxxxx'
+    ],
+    "Tues1": [
+        'https://www.xxxxxxxxxxxx.com/xxxxxxxxxxxxxxxx'
+    ],
+    "Tues2": [
+        'https://www.xxxxxxxxxxxx.com/xxxxxxxxxxxxxxxx'
+    ],
+    "Wed1": [
+        'https://www.xxxxxxxxxxxx.com/xxxxxxxxxxxxxxxx'
+    ],
+    "Fri1": [
+        'https://www.xxxxxxxxxxxx.com/xxxxxxxxxxxxxxxx'
 
+    ]
+}
 
-    #[Insert name of URL link]
-    'https://app.example.com/tenant/YourOrganization/segment',
+# Ask the user for a section name
+section = input("Enter the section name (Mon1, Tues1, Tues2, Wed1, Fri1): ").strip()
 
-    #[Insert name of URL link]
-    'https://app.example.com/tenant/YourOrganization/segment'
-        ]
-
-for urlm in urlsM:
-    webbrowser.open(urlm, new=0)
-'''
+# Open the URLs for the selected section
+if section in sections:
+    for url in sections[section]:
+        webbrowser.open(url, new=0)
+    print(f"Opened {len(sections[section])} links for {section}.")
+else:
+    print("Invalid section name. Please try again.")
